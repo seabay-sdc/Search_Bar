a486228cdc7091423f5a0e09232b34effd56553e
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-07-03 17:01:08 -0500

    created a really basic search bar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+const axios = require('axios');
 
 class App extends React.Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
   }
+  componentDidMount() {
+    this.pingServer()
+  }
+
+  pingServer () {
+    axios.get('/api/users')
+      .then((response) => {
+        //console.log(response);
+      })
+      .catch((error) => {
+        //console.log(error) //////GET RID OF ON THE CLIENT SIDE WHEN DONE!!!
+      })
+      .finally(()=>{
+        console.log("succesfully got info from the database !!! :)")
+      })
+  }
+
   render () {
-    return (<h1>Fix me!</h1>);
+    return (<div>
+      <h1>Whatchya want?</h1>
+      <input type="text"></input>
+      <button type="submit">Get it!</button>
+      </div>
+    );
   }
 }
 

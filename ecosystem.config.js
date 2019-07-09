15dbc80fd2c88ee36a4f8f71d8c8dfe914ca253e
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-07-09 16:22:54 -0500

    i am sad

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [{
-      name: 'Search_Bar',
+      name: 'SearchBar',
       script: './server.server.js'
     }],
     deploy: {
       production: {
         user: 'ubuntu',
-        //host: 'ec2-3-17-68-248.us-east-2.compute.amazonaws.com',
-        host: ["192.168.0.13"],
+        host: 'ec2-3-17-68-248.us-east-2.compute.amazonaws.com',
+        //host: ["192.168.0.13"],
         key: '~/.ssh/Search_Bar.pem',
         ref: 'origin/master',
         repo: 'https://github.com/seabay-hratx42-fec/Search_Bar.git',

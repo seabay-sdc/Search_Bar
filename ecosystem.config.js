76525d2d4f4d448fdec1f40a5f30e3a40ab83d03
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-07-09 16:37:33 -0500

    bla bla bla

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
         ref: 'origin/master',
         repo: 'https://github.com/seabay-hratx42-fec/Search_Bar.git',
         //repo: 'git@github.com:seabay-hratx42-fec/Search_Bar.git',
-        path: '/home/ubuntu',
+        path: '/home/ubuntu/Search_Bar',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
     }

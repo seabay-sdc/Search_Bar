a25b505efff45bab893da5258f0a240249fc8f99
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-07-09 16:30:55 -0500

    fucking pem keys god dammit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
         user: 'ubuntu',
         host: 'ec2-3-17-68-248.us-east-2.compute.amazonaws.com',
         //host: ["192.168.0.13"],
-        key: '~/.ssh/Search_Bar.pem',
+        key: '~/.ssh/FEC.pem',
         ref: 'origin/master',
         repo: 'https://github.com/seabay-hratx42-fec/Search_Bar.git',
         //repo: 'git@github.com:seabay-hratx42-fec/Search_Bar.git',

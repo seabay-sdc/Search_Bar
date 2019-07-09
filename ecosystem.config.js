b20042cc1149934f75e4562f70a550edb61cf52f
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-07-09 16:17:35 -0500

    shall i go amish

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,8 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'ec2-3-17-68-248.us-east-2.compute.amazonaws.com',
+        //host: 'ec2-3-17-68-248.us-east-2.compute.amazonaws.com',
+        host: ["192.168.0.13"],
         key: '~/.ssh/Search_Bar.pem',
         ref: 'origin/master',
         repo: 'https://github.com/seabay-hratx42-fec/Search_Bar.git',

8b53327a34780990b838b9df6d49ec68d12704c8
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-07-18 12:19:21 -0500

    removed failed material UI code

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Category from './Category.jsx';
-// import { Button } from 'react-bootstrap';
-import { Button, Grid, Paper, Hidden, Input, Select, FormControl, InputLabel }  from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+
+import { Button }  from '@material-ui/core';
 
 const List = props => {
     let history;
@@ -17,27 +16,6 @@ const List = props => {
           <option value={item[0]} key={i}/>
       )
   });    
-
-  const materialItem = history.map((item, i) => {
-    return(
-        <option value={item[0]} key={i}>{item[0]}</option>
-    )
-});   
-
-console.log(materialItem);
-
-  const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }));
-
-  const classes = useStyles();
 
     return (<>
 
@@ -85,6 +63,7 @@ console.log(materialItem);
   .flex-item:nth-child(2) {
       height: 40px;
       fontsize 2.5em;
+      font: 400 20px system-ui;
       order: 0;
       flex: 3 3 auto;
       align-self: center;
@@ -117,15 +96,6 @@ console.log(materialItem);
     align-self: center;
   }
 
-  .paper {
-    textAlign: center;
-    flex: 1 0 auto;
-  }
-
-  .container {
-    flexGrow: 1;
-    display: flex;
-  }
   div {
     padding: 0;
   }
@@ -217,15 +187,6 @@ console.log(materialItem);
   align-self: center;
 }
 
-.paper {
-  textAlign: center;
-  flex: 1 0 auto;
-}
-
-.container {
-  flexGrow: 1;
-  display: flex;
-}
 div {
   padding: 0;
 }
@@ -239,16 +200,12 @@ div {
     <form className="flex-container">
       <img src="https://seabay-logo.s3.us-east-2.amazonaws.com/New+Project+(1).png" className="flex-item"/>
       <input id="input" list="things"  placeholder="&#128269; Search for anything" onChange={props.handleChange} className="flex-item"/>
-      {/* <datalist id="things" className="flex-item"> */}
       <datalist id="things">
         {item}
       </datalist> 
       <Category className="flex-item" 
         searchHistory={props.searchHistory}
         setCategory={props.setCategory}/>
-      {/* <Button variant="flat" type="submit" onClick={props.submitSearch} className="flex-item">
-        Search
-      </Button> */}
 
       <Button id="smallBtn" variant="contained" onClick={props.submitSearch} className="flex-item">
         &#128269;
@@ -260,138 +217,7 @@ div {
 
     </form>
 
-    {/* <div className="container">
-      <Grid container className="container" spacing={0}>
 
-        <Hidden xsDown>
-        <Grid item sm={3} className='paper'>
-          <img src="https://seabay-logo.s3.us-east-2.amazonaws.com/New+Project+(1).png" className="flex-item"/>
-        </Grid>
-        </Hidden>
-
-        <Grid item xs={12} sm={3} className="inputField" >
-          <input id="input" list="things"  placeholder="&#128269; Search for anything" onChange={props.handleChange} className="flex-item"/>
-          <datalist id="things" className="flex-item">
-            {item}
-          </datalist> 
-        </Grid>
-
-        <Grid item xs={10} sm={3} className='paper'>
-          <Category className="flex-item" 
-            searchHistory={props.searchHistory}
-            setCategory={props.setCategory}/>
-        </Grid>
-
-        <Hidden xsDown>
-        <Grid item sm={3} className='paper'>
-          <Button id="btn" variant="contained" onClick={props.submitSearch} className="flex-item">
-            Search
-           </Button>
-        </Grid>
-        </Hidden>
-
-        <Hidden smUp>
-        <Grid item xs={2} className='paper'>
-          <Button id="btn" variant="contained" onClick={props.submitSearch} className="flex-item">
-            &#128269;
-           </Button>
-        </Grid>
-        </Hidden>
-
-      </Grid>
-    </div> */}
-
-    <div className={classes.root}>
-      <Hidden xsDown>
-      <Grid container spacing={0}>
-        <Grid item xs={2} className={classes.paper}>
-          <img src="https://seabay-logo.s3.us-east-2.amazonaws.com/New+Project+(1).png" className="flex-item"/>
-        </Grid>
-
-        <Grid item xs={4} className={classes.paper}>
-          {/* <input id="input" list="things"  placeholder="&#128269; Search for anything" onChange={props.handleChange} className="flex-item" /> */}
-          <Input  list="things"  placeholder="&#128269; Search for anything" onChange={props.handleChange} />
-          <datalist id="things" className="flex-item">
-           {item}
-          </datalist> 
-        </Grid>
-
-        <Grid item xs={4}  className={classes.paper} >
-          <Category className="flex-item" 
-            searchHistory={props.searchHistory}
-            setCategory={props.setCategory}/>
-        </Grid>
-
-        <Grid item xs={2} className={classes.paper} >
-        <Button id="btn" variant="contained" onClick={props.submitSearch} className="flex-item">
-            &#128269; Search
-           </Button>
-        </Grid>
-      </Grid>
-    </Hidden>
-
-    {/* <Hidden smUp>
-        <Grid item xs={12} className={classes.paper}>
-          <input id="input" list="things"  placeholder="&#128269; Search for anything" onChange={props.handleChange} className="flex-item" />
-          <datalist id="things" className="flex-item">
-           {item}
-          </datalist> 
-        </Grid>
-
-      <Grid container spacing={3}>
-        <Grid item xs={2} className={classes.paper}>
-          <img src="https://seabay-logo.s3.us-east-2.amazonaws.com/New+Project+(1).png" className="flex-item"/>
-        </Grid>
-
-        <Grid item xs={6}  className={classes.paper}>
-          <Category className="flex-item" 
-            searchHistory={props.searchHistory}
-            setCategory={props.setCategory}/>
-        </Grid>
-
-        <Grid item xs={3} className={classes.paper}>
-        <Button id="btn" variant="contained" onClick={props.submitSearch} className="flex-item">
-            &#128269;
-           </Button>
-        </Grid>
-      </Grid>
-    </Hidden> */}
-    </div>
-
-    <div>
-    {/* <Grid container spacing={0}>
-    <Grid item xs={6}  className={classes.paper}>
-    <Select
-          classes={useStyles()}
-          placeholder="Search a country (start with a)"
-          options={materialItem}
-          value='cats'
-        >
-        {materialItem}
-          </Select>
-        </Grid>
-    </Grid> */}
-
-    {/* <FormControl className={classes.formControl} required >
-                                        <InputLabel htmlFor="name">Name</InputLabel>
-                                        <Select
-                                            native
-                                            required
-                                            // value={this.state.name}
-                                            // onChange={this.handleChange}
-                                            inputProps={{
-                                                name: 'name',
-                                                id: 'name'
-                                            }}
-                                        >
-                                          {materialItem}
-                                            <option value="" />
-                                            <option value={"lala"}>lala</option>
-                                            <option value={"lolo"}>lolo</option>
-                                        </Select>
- </FormControl> */}
-
-    </div>
     </>)
 
 }

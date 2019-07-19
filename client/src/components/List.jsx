7128620112c21a4596d5164d9590d5c7b38635b7
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-07-19 16:55:49 -0500

    added {props.submitSearch} to form action

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -197,7 +197,7 @@ div {
     `}
   </style>
 
-    <form className="flex-container" onSubmit={props.submitSearch} action=".">
+    <form className="flex-container" onSubmit={props.submitSearch} action={props.submitSearch}>
       <img src="https://seabay-logo.s3.us-east-2.amazonaws.com/New+Project+(1).png" className="flex-item"/>
       <input id="input" list="things"  placeholder="&#128269; Search for anything" onChange={props.handleChange} className="flex-item" type="search" onsearch={props.submitSearch}/>
       <datalist id="things">

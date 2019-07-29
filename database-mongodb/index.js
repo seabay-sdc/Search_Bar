88b942f97798d1879f2f8fc3d2a4293c07cd3c61
Trevor Cannon <cannon.trevor@gmail.com>
2019-07-29 17:03:56 -0500

    minor changes to routes for MongoDB

--- database-mongodb/index.js
+++ database-mongodb/index.js
@@ -5,14 +5,18 @@ let dbo;
 const client = MongoClient.connect("mongodb://localhost:27017/SDC", { useNewUrlParser: true }, function(err,db) {
   if (err) {throw err};
   
-  console.log("connected ya silly goose");
+  console.log("connected to MongoDB locally ya silly goose");
   dbo = db.db("SDC");
 })
 const getOne = () => {
-  console.log('i got hit');
+  console.log('On my way to snag one');
  return dbo.collection("seabayItems").findOne({"_id": 5})
 };
 
+const getEverything = () => {
+  console.log('everything will be mine');
+  return dbo.collection("seabayItems").find()
+};
 
 
-module.exports = { getOne }
+module.exports = { getOne, getEverything }
